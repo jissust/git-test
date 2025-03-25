be0db5ed5272f252a3800942e78680d93673795a
jesustissera <jesustissera@gmail.com>
2025-03-24 22:56:00 -0300

    I add validations and validation style

--- conference-ticket-generator-main/src/App.tsx
+++ conference-ticket-generator-main/src/App.tsx
@@ -1,15 +1,14 @@
-import React, { useState } from 'react'
-import './App.css'
-import patternLines from './assets/images/pattern-lines.svg'
-import patternCircle from './assets/images/pattern-circle.svg'
-import patternSquigglyLineTop from './assets/images/pattern-squiggly-line-top.svg'
-import patterSquigglyLineBottomDesktop from './assets/images/pattern-squiggly-line-bottom-desktop.svg'
-import iconUpload from './assets/images/icon-upload.svg'
-import logoFull from './assets/images/logo-full.svg'
-import iconInfo from './assets/images/icon-info.svg'
+import React, { useState } from "react";
+import "./App.css";
+import patternLines from "./assets/images/pattern-lines.svg";
+import patternCircle from "./assets/images/pattern-circle.svg";
+import patternSquigglyLineTop from "./assets/images/pattern-squiggly-line-top.svg";
+import patterSquigglyLineBottomDesktop from "./assets/images/pattern-squiggly-line-bottom-desktop.svg";
+import iconUpload from "./assets/images/icon-upload.svg";
+import logoFull from "./assets/images/logo-full.svg";
+import { IconInfo } from "./components/Icons";
 
 function App() {
-  
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -24,98 +23,154 @@ function App() {
     setFormData({ ...formData, [name]: value });
   };
 
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+
+    if (file) {
+      const validTypes = ["image/jpeg", "image/jpg", "image/png"];
+
+      if (!validTypes.includes(file.type)) {
+        setErrors((prev) => ({
+          ...prev,
+          file: "Invalid file type.",
+        }));
+        setFormData({ ...formData, file: null });
+        return;
+      }
+
+      if (file.size > 500 * 1024) {
+        setErrors((prev) => ({
+          ...prev,
+          file: "File too large.",
+        }));
+        setFormData({ ...formData, file: null });
+        return;
+      }
+
+      // Si pasa todas las validaciones:
+      setErrors((prev) => ({ ...prev, file: "" }));
+      setFormData({ ...formData, file });
+    }
+  };
+
+  const validate = () => {
+    const tempErrors = {};
+    if (!formData.name.match(/^[A-Za-z\s]+$/))
+      tempErrors.name = "Name should contain only letters";
+    if (formData.name.length == 0) tempErrors.name = "Name cannot be empty";
+    if (!formData.email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/))
+      tempErrors.email = "Invalid email format";
+    if (formData.email.length == 0) tempErrors.email = "Email cannot be empty";
+    setErrors(tempErrors);
+    return Object.keys(tempErrors).length === 0;
+  };
+
   const handleSubmit = (e) => {
-    e.eventPreventDefault()
-    console.log(formData)
-  }
+    e.preventDefault();
+    if (validate()) {
+      console.log(formData);
+    } else {
+      console.log(errors);
+    }
+  };
 
   return (
     <>
+      <img className="pattern-lines" src={patternLines} alt="" />
+      <img className="pattern-circle" src={patternCircle} alt="" />
       <img
-    className="pattern-lines"
-      src={patternLines}
-      alt=""
-    />
-    <img
-    className="pattern-circle"
-      src={patternCircle}
-      alt=""
-    />
-    <img
-    className="pattern-squiggly-line-top"
-      src={patternSquigglyLineTop}
-      alt=""
-    />
-    <img
-    className="pattern-squiggly-line-bottom-desktop"
-      src={patterSquigglyLineBottomDesktop}
-      alt=""
-    />
-    <main>
-      <nav>
-        <img src={logoFull} alt="" />
-      </nav>
-      <h1>Your Journey to Coding Conf 2025 Starts Here!</h1>
-      <p>Secure your spot at next year's biggest coding conference.</p>
+        className="pattern-squiggly-line-top"
+        src={patternSquigglyLineTop}
+        alt=""
+      />
+      <img
+        className="pattern-squiggly-line-bottom-desktop"
+        src={patterSquigglyLineBottomDesktop}
+        alt=""
+      />
+      <main>
+        <nav>
+          <img src={logoFull} alt="" />
+        </nav>
+        <h1>Your Journey to Coding Conf 2025 Starts Here!</h1>
+        <p>Secure your spot at next year's biggest coding conference.</p>
 
-      <form onSubmit={handleSubmit}>
-        <div className="form-row">
-          <label htmlFor=""> Upload Avatar </label>
-          <div className="form-container-focus">
-          <div className="form-input-file">
-            <div className="form-input-file-content">
-              <div className="form-container-logo-input">
-                <img src={iconUpload} alt="" />
+        <form onSubmit={handleSubmit}>
+          <div className="form-row">
+            <label htmlFor=""> Upload Avatar </label>
+            <div className="form-container-focus">
+              <div className="form-input-file">
+                <div className="form-input-file-content">
+                  <div className="form-container-logo-input">
+                    <img src={iconUpload} alt="" />
+                  </div>
+                  <div className="form-input-file-content__text">
+                    <div>Drag and drop or click to upload</div>
+                  </div>
+                </div>
+                <input type="file" name="file" onChange={handleFileChange} />
               </div>
-              <div className="form-input-file-content__text">
-                <div>Drag and drop or click to upload</div>
-              </div>
+              <div className="border-focus"></div>
             </div>
-            <input type="file" name=""  />
+            <div
+              className={`form-input-file__text ${errors.file ? "error" : ""}`}
+            >
+              <IconInfo color={errors.file ? "#c98286" : "#D1D0D5"} />
+              {errors.file && <span>{errors.file}</span>}
+              <span>Upload your photo (JPG or PNG, max size: 500KB).</span>
+            </div>
           </div>
-          <div className="border-focus"></div>
-        </div>
-          <div className="form-input-file__text">
-            <img src={iconInfo} alt="" />
-            <div>Upload your photo (JPG or PNG, max size: 500KB).</div>
+          <div className="form-row margin-bottom-first">
+            <label htmlFor=""> Full Name </label>
+            <div className="form-container-focus">
+              <input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                className={errors.name ? "border-error" : ""}
+              />
+              <div className="border-focus"></div>
+            </div>
+            {errors.name && <p className="error">{errors.name}</p>}
+            <img className="pattern-circle-2" src={patternCircle} alt="" />
           </div>
-        </div>
-        <div className="form-row margin-bottom-first">
-          <label htmlFor=""> Full Name </label>
-          <div className="form-container-focus">
-            <input type="text" name="name" value={formData.name} onChange={handleChange} />
+          <div className="form-row margin-bottom-first">
+            <label htmlFor=""> Email Address </label>
+            <div className="form-container-focus">
+              <input
+                type="text"
+                name="email"
+                value={formData.email}
+                placeholder="example@email.com"
+                onChange={handleChange}
+                className={errors.email ? "border-error" : ""}
+              />
+              <div className="border-focus"></div>
+            </div>
+            {errors.email && <p className="error">{errors.email}</p>}
+          </div>
+          <div className="form-row margin-bottom-first">
+            <label htmlFor=""> GitHub Username </label>
+            <div className="form-container-focus">
+              <input
+                type="text"
+                name="github"
+                value={formData.github}
+                placeholder="@yourusername"
+                onChange={handleChange}
+              />
+              <div className="border-focus"></div>
+            </div>
+          </div>
+          <div className="form-container-focus margin-top-second">
+            <button className="btn-primary">Generate My Ticket</button>
             <div className="border-focus"></div>
           </div>
-          <img
-          className="pattern-circle-2"
-            src={patternCircle}
-            alt=""
-          />
-        </div>
-        <div className="form-row margin-bottom-first">
-          <label htmlFor=""> Email Address </label>
-          <div className="form-container-focus">
-            <input type="text" name="email" value={formData.email} placeholder="example@email.com" onChange={handleChange} />
-            <div className="border-focus"></div>
-          </div>
-        </div>
-        <div className="form-row margin-bottom-first">
-          <label htmlFor=""> GitHub Username </label>
-          <div className="form-container-focus">
-            <input type="text" name="github" value={formData.github} placeholder="@yourusername" onChange={handleChange} />
-            <div className="border-focus"></div>
-          </div>
-        </div>
-        <div className="form-container-focus margin-top-second">
-          <button  className="btn-primary">
-            Generate My Ticket
-          </button>
-          <div className="border-focus"></div>
-        </div>
-      </form>
-    </main>
+        </form>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
